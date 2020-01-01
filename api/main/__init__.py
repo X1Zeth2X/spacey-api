c5c444e5882b044cc6e880ea61021565c714a822
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-02 03:42:47 +0400

    Use getenv for loading config.

--- api/main/__init__.py
+++ api/main/__init__.py
@@ -1,6 +1,8 @@
 ## Main
 
 import logging
+
+from os import getenv
 from flask import Flask
 
 # Import extensions
@@ -15,7 +17,7 @@ static_url_path = basedir + "/static"
 
 def create_app(config_name):
     app = Flask(__name__, static_url_path=static_url_path)
-    app.config.from_object(config_by_name[config_name])
+    app.config.from_object(getenv('APP_SETTINGS', config_by_name[config_name]))
 
     ## Add logger
     logging.basicConfig(
