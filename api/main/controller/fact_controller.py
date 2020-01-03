cf50b2f4f72d048f003ec47f5c87053d576f2f95
Zeth Leonardo <zethiscool11@gmail.com>
2020-01-03 19:20:16 +0400

    Updated routes and added services.

--- api/main/controller/fact_controller.py
+++ api/main/controller/fact_controller.py
@@ -11,21 +11,51 @@ api = FactDto.api
 _payload = FactDto.create_fact
 
 
-@api.route("/get/<string:fact_public_id>")
+@api.route("/<string:fact_public_id>")
 class FactGet(Resource):
-    @api.doc("Get a specific fact using its public id.", responses={})
-    @jwt_required
+    @api.doc(
+        "Get a specific fact using its public id.",
+        responses={
+            # To be added
+        },
+    )
     def get(self, fact_public_id):
-        """ """
-        pass
+        return FactService.get(fact_public_id)
 
 
 @api.route("/create")
 class FactCreate(Resource):
     @api.expect(_payload, validate=True)
-    @api.doc("Add a new fact.", responses={})
+    @api.doc(
+        "Add a new fact.",
+        responses={
+            # To be added
+        },
+    )
     @jwt_required
     def post(self):
         current_user = get_user(get_jwt_identity())
         data = request.get_json()
         return FactService.create(data, current_user)
+
+
+@api.route("/delete/<string:fact_public_id>")
+class FactDelete(Resource):
+    @api.doc(
+        "Delete a specific fact using its public id.",
+        responses={
+            # To be added
+        },
+    )
+    def delete(self, fact_public_id):
+        current_user = get_user(get_jwt_identity())
+        return FactService.delete(fact_public_id, current_user)
+
+
+@api.route("/update/<string:fact_public_id>")
+class FactUpdate(Resource):
+    @api.doc("Delete a specific fact using its public id.")
+    def put(self, fact_public_id):
+        current_user = get_user(get_jwt_identity())
+        data = request.get_json()
+        return FactService.update(fact_public_id, data, current_user)
